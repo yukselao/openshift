f6ff5eee938820ce0798176b4f9d8cd0f3bf9b4d
root <root@lbint.security.tr.ibm.com>
2020-07-29 23:38:11 +0300

    commit

---
+++ get-openshift.sh
@@ -0,0 +1,5 @@
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.3.29/openshift-install-linux-4.3.29.tar.gz
+
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.3.29/openshift-client-linux-4.3.29.tar.gz
+
+wget https://rpmfind.net/linux/epel/8/Everything/x86_64/Packages/s/screen-4.6.2-10.el8.x86_64.rpm
