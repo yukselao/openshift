11bd507df914686465c654e421f68996157d1a35
root <root@lbint.security.tr.ibm.com>
2020-07-29 23:44:34 +0300

    commit

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -2,5 +2,6 @@ mkdir ~/bin
 cd /tmp/
 export TERRAFORM_VERSION=0.11.14
 curl -O -L https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d ~/bin/
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /opt/openshift/bin/
+ln -s /opt/openshift/bin/terraform /usr/local/bin/terraform
 terraform -v
